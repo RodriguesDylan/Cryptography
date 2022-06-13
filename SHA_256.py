83affc36a9e44466edf46cbd38670539adfaca18
Raikirin <91314577+RodriguesDylan@users.noreply.github.com>
2022-06-13 16:55:44 +0200

    Corrected padding, added time, added salt

--- SHA_256.py
+++ SHA_256.py
@@ -4,7 +4,8 @@
 #
 # 2022 Dylan Rodrigues
 from hashlib import sha256
-# Used to compare lib with homemade
+from secrets import token_hex
+import time
 
 # Initialize array of round constants :
 # (first 32 bits of the fractional parts of the cube roots of the first 64 primes 2...311)
@@ -76,41 +77,41 @@ def generate_hash(message: bytearray) -> bytearray:
                             2**32).to_bytes(4, 'big')
                 message_schedule.append(schedule)
 
-    assert len(message_schedule) == 64
+        assert len(message_schedule) == 64
 
-    # Initialize working variables
-    a = h0
-    b = h1
-    c = h2
-    d = h3
-    e = h4
-    f = h5
-    g = h6
-    h = h7
+        # Initialize working variables
+        a = h0
+        b = h1
+        c = h2
+        d = h3
+        e = h4
+        f = h5
+        g = h6
+        h = h7
 
-    # Compression function main loop
-    for t in range(64):
-        t1 = ((h + _capsigma1(e) + _ch(e, f, g) +
-               K[t] + int.from_bytes(message_schedule[t], 'big')) % 2**32)
-        t2 = (_capsigma0(a) + _maj(a, b, c)) % 2**32
-        h = g
-        g = f
-        f = e
-        e = (d + t1) % 2**32
-        d = c
-        c = b
-        b = a
-        a = (t1 + t2) % 2**32
+        # Compression function main loop
+        for t in range(64):
+            t1 = ((h + _capsigma1(e) + _ch(e, f, g) +
+                   K[t] + int.from_bytes(message_schedule[t], 'big')) % 2**32)
+            t2 = (_capsigma0(a) + _maj(a, b, c)) % 2**32
+            h = g
+            g = f
+            f = e
+            e = (d + t1) % 2**32
+            d = c
+            c = b
+            b = a
+            a = (t1 + t2) % 2**32
 
-    # Add the compressed chunk to the current hash value
-    h0 = (h0 + a) % 2**32
-    h1 = (h1 + b) % 2**32
-    h2 = (h2 + c) % 2**32
-    h3 = (h3 + d) % 2**32
-    h4 = (h4 + e) % 2**32
-    h5 = (h5 + f) % 2**32
-    h6 = (h6 + g) % 2**32
-    h7 = (h7 + h) % 2**32
+        # Add the compressed chunk to the current hash value
+        h0 = (h0 + a) % 2**32
+        h1 = (h1 + b) % 2**32
+        h2 = (h2 + c) % 2**32
+        h3 = (h3 + d) % 2**32
+        h4 = (h4 + e) % 2**32
+        h5 = (h5 + f) % 2**32
+        h6 = (h6 + g) % 2**32
+        h7 = (h7 + h) % 2**32
 
     # Produce the final hash value (big-endian)
     return ((h0).to_bytes(4, 'big') + (h1).to_bytes(4, 'big')
@@ -169,8 +170,29 @@ def _capsigma1(x: int):
     return res
 
 
+def salty_hash(message: str, nb_bits: int = 32):
+    random_string = token_hex(nb_bits)
+    salted_message = message+random_string
+    return salted_message
+
+
 if __name__ == "__main__":
-    message=input("Enter the message to hash : ")
-    print("Hash using own SHA-256 :\n", generate_hash(message).hex())
-    print("Hash using hashlib sha256 :\n", sha256(
-        message.encode('utf-8')).hexdigest())
+    message = input("Enter the message to hash : ")
+    choice = input("Ajouter du sel avec le message? (Y/N) ")
+
+    if (choice.casefold() == 'y'):
+        salted_message = salty_hash(message)
+        #print(salted_message, "\n")
+        start_time = time.time()
+        print("Hash using own SHA-256 salted :\n",
+              generate_hash(salted_message).hex())
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+        start_time = time.time()
+        print("Hash using hashlib sha256 salted :\n", sha256(
+            salted_message.encode('utf-8')).hexdigest())
+        print("--- %s seconds ---" % (time.time() - start_time))
+    else:
+        print("Hash using own SHA-256 :\n", generate_hash(message).hex())
+        print("Hash using hashlib sha256 :\n", sha256(
+            message.encode('utf-8')).hexdigest())
